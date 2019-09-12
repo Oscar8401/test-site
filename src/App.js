047476a20f9970b7c00e1c3501d42f968df89327
Taleb_84 <taleb_n1@hotmail.com>
2019-09-12 09:40:06 +0200

    create test-site project, add style, update view

---
+++ src/App.js
@@ -0,0 +1,105 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react';
+import { Col, Row, InputGroup, FormControl, ButtonToolbar, Button } from 'react-bootstrap'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeroBackground from './assest/images/HeroBackground.png';
+
+function App() {
+  const images = [1, 2, 3, 4, 5, 6, 7, 8];
+  return (
+    <div className="App">
+      <section>
+        <div className="style-one">
+          <div className="style-two">
+            <img src={HeroBackground} className="Hero-background" alt="HeroBackground" />
+          </div>
+          <div className="style-three">
+            <Row >
+              <Col sm={6}>
+                <div className="title">Hello, and <b>welcome</b></div>
+                <div className="sub-title">This is test site</div>
+              </Col>
+            </Row>
+            <Row className="image-over-button">
+              <Col>
+                <ButtonToolbar>
+                  <Button id="button" variant="warning">Learn more</Button>
+                </ButtonToolbar>
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <Row>
+          <Col>
+            <div className="about-us">About us</div>
+          </Col>
+        </Row>
+        <Row>
+          <div className="about-us-container">
+            <Col sm={8}>
+              <img src={require(`./assest/images/circle.png`)} className="about-us-img" alt="HeroBackground" />
+            </Col>
+            <Col sm={8}>
+              <p className="about-us-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.
+              </p>
+            </Col>
+            <Col sm={8}>
+              <p className="about-us-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.
+              </p>
+            </Col>
+          </div>
+        </Row>
+
+      </section>
+
+      <section className="section-three">
+        <Row>
+          <Col><div className="about-us">Projects</div></Col>
+        </Row>
+        <Row>
+          {images.map((num, index) => {
+            return <Col sm={3}><img src={require(`./assest/images/Image${num}.png`)} className="project-img" alt="project image" /></Col>
+          })}
+        </Row>
+      </section>
+      <section className="section-Four">
+        <Row>
+          <Col><div className="about-cotact-us">Contact</div></Col>
+        </Row>
+        <Row>
+          <Col sm={6}>
+            <InputGroup className="mb-3">
+              <FormControl placeholder="Name" aria-label="Amount (to the nearest dollar)"/>
+            </InputGroup>
+          </Col>
+          <Col sm={6}>
+            <InputGroup className="mb-3">
+              <FormControl placeholder="Email" aria-label="Amount (to the nearest dollar)"/>
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <InputGroup className="mb-3">
+              <FormControl placeholder="Message" aria-label="Amount (to the nearest dollar)" as="textarea" rows="3"/>
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ButtonToolbar>
+              <Button id="normal-button" variant="warning">Send message</Button>
+            </ButtonToolbar>
+          </Col>
+        </Row>
+      </section>
+    </div>
+  );
+}
+export default App;
